79409adf64852501e8895f8ee3b228ef3b3743ec
nishchithas <snishchitha.mysore@gmail.com>
2020-01-13 19:15:45 +0000

    Create about.js

---
+++ sr/navigation/about.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import '../App.css'
+
+const About = () => {
+    return (
+        <div className="about-box">
+            <h1>About Food Recipe</h1>
+            <p>
+                A recipe is a set of instructions that describes how to prepare or make something, especially a dish of prepared food.
+                The term recipe is also used in medicine or in information technology (e.g., user acceptance). A doctor will usually begin a prescription with recipe, Latin for take, usually abbreviated as Rx or the equivalent symbol (℞).
+                Contents
+                1	History
+                1.1	Early examples
+                1.2	Modern recipes and cooking advice
+                2	Components
+                3	Internet and television recipes
+                4	Recipe design tools
+                5	See also
+                6	References
+                7	External links
+                History
+                Early examples
+                Apicius, De re culinaria, an early collection of recipes.
+                The earliest known written recipes date from approximately 1600 BC and come from an Akkadian tablet from southern Babylonia.[1] There are also works in ancient Egyptian hieroglyphs depicting the preparation of food.[2]
+                Many ancient Greek recipes are known. Mithaecus's cookbook was an early one, but most of it has been lost; Athenaeus quotes one short recipe in his Deipnosophistae. Athenaeus mentions many other cookbooks, all of them lost.[3]
+                Roman recipes are known starting in the 2nd century BCE with Cato the Elder's De Agri Cultura. Many authors of this period described eastern Mediterranean cooking in Greek and in Latin.[3] Some Punic recipes are known in Greek and Latin translation.[3]
+                The large collection of recipes De re coquinaria, conventionally titled Apicius, appeared in the 4th or 5th century and is the only complete surviving cookbook from the classical world.[3] It lists the courses served in a meal as Gustatio (appetizer), Primae Mensae (main course) and Secundae Mensae (dessert).[4] Each recipe begins with the Latin command "Take...," "Recipe...."[5]
+                Arabic recipes are documented starting in the 10th century; see al-Warraq and al-Baghdadi.
+                The earliest recipe in Persian dates from the 14th century. Several recipes have survived from the time of Safavids, including Karnameh (1521) by Mohammad Ali Bavarchi, which includes the cooking instruction of more than 130 different dishes and pastries, and Madat-ol-Hayat (1597) by Nurollah Ashpaz.[6] Recipe books from the Qajar period are numerous, the most notable being Khorak-ha-ye Irani by prince Nader Mirza.[7]
+                King Richard II of England commissioned a recipe book called Forme of Cury in 1390,[8] and around the same time, another book was published entitled Curye on Inglish, "cury" meaning cooking.[9] Both books give an impression of how food for the noble classes was prepared and served in England at that time. The luxurious taste of the aristocracy in the Early Modern Period brought with it the start of what can be called the modern recipe book. By the 15th century, numerous manuscripts were appearing detailing the recipes of the day. Many of these manuscripts give very good information and record the re-discovery of many herbs and spices including coriander, parsley, basil and rosemary, many of which had been brought back from the Crusades.[10]
+            </p>
+            <h1>Nutrition Analysis</h1>
+            <p>
+                Nutrition analysis refers to the process of determining the nutritional content of foods and food products. The process can be performed through a variety of certified methods.
+                Methods
+                Laboratory analysis
+                Traditionally, food companies would send food samples to laboratories for physical testing. Typical analysis includes: Moisture (water) by loss of mass at 102 °C, Protein by analysis of total nitrogen, either by Dumas or Kjeldahl methods, Total fat, traditionally by a solvent extraction, but often now by secondary methods such as NMR, Crude ash (total inorganic matter) by combustion at 550C, Estimated dietary fibre by various AOAC methods such as 985.29, Sodium (and thereby Salt) either by flame photometry, AA or ICP-OES, Total sugars, normally by a liquid chromatography technique, such as IC-HPAED or HPLC-RI, Fatty acids by GC-FID, Carbohydrates and energy values are normally calculated from these analytical values.
+
+                Software
+                Software is available as an alternative to laboratory nutrition analysis. This software typically utilizes a database of ingredients that have previously been laboratory tested. The user can input ingredient data by matching their ingredients to ingredients found in the database; the analysis can then be calculated.
+
+                Online nutrition analysis
+                In recent years, web-based nutrition analysis software services have become more popular. Online nutrition analysis allows users to access online databases and draw from certified ingredients to produce instant nutrition information.
+
+                Turnkey nutrition analysis services
+                Another emerging trend is the use of nutritional analysis services that do a complete analysis of any recipe by using their proprietary database. Users provide recipes, cooking methods and serving sizes. In turn, the service provides a complete nutritional analysis.
+
+                Applications
+                In the United States, nutrition information is required on packaged retail foods in the form of nutrition facts panels as a result of food labeling regulations.[1] In recent years, many restaurants have begun posting nutrition information as a result of both customer demand and menu-labeling laws.[2]
+
+                Nutrition facts label
+                Main article: Nutrition facts label
+                Menu-labeling
+                The Patient Protection and Affordable Care Act, signed into law March 23, 2010, includes a provision that creates a national, uniform nutrition-disclosure standard for food service establishments.
+
+                The nutrition-disclosure provision requires chain restaurants, similar retail food establishments and vending machines with 20 or more locations to provide specific nutrition labeling information. Those establishments must post calories on menus, menu boards and drive-thru boards. Buffets, salad bars and other self-service items are also included and will be required to provide caloric information adjacent to the item.[3]
+
+                Recently many state and local menu-labeling laws have been passed requiring restaurants to post nutrition information on menus and menu boards, or have it readily available upon customer request.[4] Restaurants have had to perform nutrition analysis in order to generate nutrition information and conform to these laws. More recently national legislation has been introduced that would set a national standard for menu labeling, the most popular of which is the LEAN act.
+            </p>
+        </div>
+    );
+}
+
+export default About
